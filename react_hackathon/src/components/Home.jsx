3df72c36c30b0a033d29fd0159431c126e34d9be
BlenTesfaye <blent3124@gmail.com>
2023-11-01 12:23:23 -0400

    functioning searchbar

--- react_hackathon/src/components/Home.jsx
+++ react_hackathon/src/components/Home.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import Search from './Search';
+
+
 const Home =()=>{
 
     return(
@@ -7,6 +11,11 @@ const Home =()=>{
             <h2>But make it sexy.</h2>
             <h3>Welcome to CRAVE.  Find a meal to satisfy what you're craving. Choose a category to see a selection of recipes to satisfy your appetite.</h3>
             <h2>What do you crave?</h2>
+
+
+            {/* Add the Search component here */}
+      <Search />
+
         </div>
     )
 }
