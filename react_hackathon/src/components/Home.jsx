d3379a3087ee11cfaed999a00d56e8f53998eeb4
Shanda Shaw <srh0029@hotmail.com>
2023-11-01 10:02:33 -0500

    Code cleanup, return to CategoryDetails link from IndividualRecipe component page,  playing with name/homepage

--- react_hackathon/src/components/Home.jsx
+++ react_hackathon/src/components/Home.jsx
@@ -2,8 +2,11 @@ const Home =()=>{
 
     return(
         <div>
-
-            <p>Hello Home</p>
+            <h1>CRAVE</h1>
+            <h2>Dinner.</h2>
+            <h2>But make it sexy.</h2>
+            <h3>Welcome to CRAVE.  Find a meal to satisfy what you're craving. Choose a category to see a selection of recipes to satisfy your appetite.</h3>
+            <h2>What do you crave?</h2>
         </div>
     )
 }
--- react_hackathon/src/components/Main.jsx
+++ react_hackathon/src/components/Main.jsx
@@ -2,38 +2,22 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
 
 import IndividualRecipe from './IndividualRecipe'
-// import Categories from './Categories'
-
 import CategoriesList from './Categories'
 import CategoryDetails from './CategoryDetails'
-
 
 const Main=()=>{
 return(
     <div className="routes-container">
       <Routes>
       
-      <Route  path="/" element={<Home />} />
-
-      <Route  path="/categories" element={<CategoriesList/>} />
-      <Route path="/categories/:cat" element={<CategoryDetails />}/>
-      {/* <Route  path="/individualrecipe/:id" element={<IndividualRecipe  />} /> */}
-
-
-      {/* <Route  path="/categorydetails" element={<CategoryDetails  />} /> */}
-      {/* <Route path="/categories/:id" element={<StarShipPage />}/> */}
-
-      <Route  path="/individualrecipe/:id" element={<IndividualRecipe  />} />
+      <Route path="/" element={<Home/>} />
+      <Route path="/categories" element={<CategoriesList/>} />
+      <Route path="/categories/:cat" element={<CategoryDetails/>} />
+      <Route path="/individualrecipe/:id" element={<IndividualRecipe/>} />
       
-
-      
-      {/* <Route path="/categories/:id" element={<StarShipPage />}/> */}
-
-
       </Routes>
     </div>
 )
-
 }
 
 export default Main
